964c859f5093305cabc92703655d51005245393a
Hanna Lee <mani21na@gmail.com>
2019-08-16 19:23:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,3 +8,13 @@ def prime? (num)
         (2..num/2).none? { |i| num % i == 0}
     end
 end
+
+def prime?(num)
+  if num < 0 or num == 0 or num == 1
+    return false
+  else
+    (2..num-1).to_a.all? do |possible_factor|
+      num % possible_factor != 0
+    end
+  end
+end
